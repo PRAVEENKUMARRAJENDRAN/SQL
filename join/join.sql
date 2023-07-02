c2f65c221005a1b4b054287ce4d3cd5020f5ed1c
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2023-07-02 17:21:13 -0400

    join statement

--- join/join.sql
+++ join/join.sql
@@ -4,13 +4,13 @@
 
 -- SYNTAX 
 SELECT * FROM table1 t1 JOIN table2 t2
-ON t1.columnname = t2.columnname
+ON t1.columnname = t2.columnname;
 
 
 -- EXAMPLE: Print the prodname based on the order details and order
-SELECT p.productname FROM products p join orderdetails od join orders o 
+SELECT * FROM products p join orderdetails od join orders o 
 ON p.productid = od.productid AND 
-od.orderid =  o.orderid
+od.orderid =  o.orderid;
 
 -- when I performed this join statement I got products with order id. 
 
@@ -22,5 +22,16 @@ od.orderid =  o.orderid
 SELECT columnnames FROM table1 t1 
 INNER JOIN 
 table2 t1
-ON t1.columnname = t1.columnname
+ON t1.columnname = t1.columnname;
 
+-- bit confusing since it has more data
+-- let's take table  categories and products
+
+SELECT p.productname, c.Description FROM categories c JOIN products p ON c.categoryid = p.categoryid;
+
+-- SELECT p.productname, c.Description, COUNT(DISTINCT(c.CategoryName)) FROM categories c JOIN products p ON c.categoryid = p.categoryid;
+-- Error Code: 1140. In aggregated query without GROUP BY, expression #1 of SELECT list contains nonaggregated column 'northwind.p.ProductName'; this is incompatible with sql_mode=only_full_group_by
+
+
+-- we are going to find number of order placed by a customer using aggegerate and coutn function
+SELECT c.customername, COUNT(o.OrderID) FROM customers c JOIN orders o ON c.CustomerID = o.CustomerID GROUP BY c.CustomerID;
