3ebcacc8ab1fdc0afe8ef59d0a0770e4a4d8aa71
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2023-07-04 15:36:12 -0400

    View Statements

---
+++ Project/ECOMMERCE/View_Creation/basic_view_creation.sql
@@ -0,0 +1,10 @@
+-- View is a virtual table
+-- THe complex queries can be written with the view statement
+-- THe join, union and all type of condition can be written with View 
+
+
+-- SYNTAX
+
+CREATE VIEW "VIEW NAME" AS
+SELECT tn.columnanme1, tn.columname2 FROM tabkename tn 
+
